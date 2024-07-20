6762af7a8dc2ead7b18051d98786465638924f1b
amalkuriakose <amalkuriakose802@gmail.com>
2024-07-20 08:45:24 +0530

    added tests

--- test_hello.py
+++ test_hello.py
@@ -3,4 +3,4 @@ from hello import app
 def test_hello():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"<h2>Hello, World!</h2>" in response.data
+    assert b"<h2>Hello, World</h2>" in response.data
